fd197c2a53e3b868e7841e6120e43a5ff804ec61
Pablo Lima Dias <pabloidz@gmail.com>
2018-10-25 19:33:58 -0300

    Picking Numbers
    https://www.hackerrank.com/challenges/picking-numbers/problem

---
+++ implementation/pickingNumbers.js
@@ -0,0 +1,17 @@
+function pickingNumbers(arr) {
+    const totals = arr.reduce((prev, curr) => {
+        prev[curr] = ((prev[curr] || 0) + 1);
+        return prev;
+    }, {});
+    let maxNumber = 0;
+    Object.keys(totals).sort().forEach((value, index, arr) => {
+        if (index === 0) {
+            maxNumber = totals[value];
+        }
+        if ((index + 1 < arr.length) && (Math.abs(value - arr[index + 1]) === 1)) {
+            maxNumber = Math.max(maxNumber, totals[value] + totals[arr[index + 1]]);
+        }
+    })
+    return maxNumber;
+}
+module.exports = pickingNumbers;
---
+++ implementation/pickingNumbers.test.js
@@ -0,0 +1,23 @@
+const pickingNumbers = require('./pickingNumbers');
+
+test('testcase 0', () => {
+    expect(pickingNumbers([4, 6, 5, 3, 3, 1])).toBe(3);
+});
+
+test('testcase 1', () => {
+    expect(pickingNumbers([1, 2, 2, 3, 1, 2])).toBe(5);
+});
+
+test('testcase 2', () => {
+    expect(pickingNumbers(Array(100).fill(66))).toBe(100);
+});
+
+test('testcase 7', () => {
+    expect(pickingNumbers([
+        4, 97, 5, 97, 97, 4, 97, 4, 97, 97, 97, 97, 4, 4, 5, 5, 97, 5, 97, 99, 
+        4, 97, 5, 97, 97, 97, 5, 5, 97, 4, 5, 97, 97, 5, 97, 4, 97, 5, 4, 4, 
+        97, 5, 5, 5, 4, 97, 97, 4, 97, 5, 4, 4, 97, 97, 97, 5, 5, 97, 4, 97, 
+        97, 5, 4, 97, 97, 4, 97, 97, 97, 5, 4, 4, 97, 4, 4, 97, 5, 97, 97, 97, 
+        97, 4, 97, 5, 97, 5, 4, 97, 4, 5, 97, 97, 5, 97, 5, 97, 5, 97, 97, 97
+    ])).toBe(49);
+});

05e2e1a5d627d0a6e47e399c145b71dafc6d8195
Pablo Lima Dias <pabloidz@gmail.com>
2018-11-01 22:28:24 -0300

    Using ES6 to solve Between Two Sets

--- implementation/betweenTwoSets.js
+++ implementation/betweenTwoSets.js
@@ -1,29 +1,14 @@
 function betweenTwoSets(a, b) {
-
-    let x = [];
-
-    for (let i = 1; i <= 100; i++) {
-        let restoZero = true;
-
-        a.forEach(item => {
-            if (i % item !== 0) {
-                restoZero = false;
-            }
-        });
-
-        b.forEach(item => {
-            if (item % i !== 0) {
-                restoZero = false;
-            }
-        });
-
-        if (restoZero) {
-            x.push(i);
-        }
-
+    const maxA = a.reduce((prev, curr) => Math.max(prev, curr), 0);
+    const minB = b.reduce((prev, curr) => Math.min(prev, curr), Number.MAX_VALUE);
+    const conditionA = num => a.every(item => num % item === 0);
+    const conditionB = num => b.every(item => item % num === 0);
+    const total = minB - maxA + 1
+    if (total < 0) {
+        return 0;
     }
-
-    return x.length;
-
+    return Array.from({length: total}, (v, k) => k + maxA)
+        .filter(num => conditionA(num) && conditionB(num))
+        .length;
 }
 module.exports = betweenTwoSets;
--- implementation/betweenTwoSets.test.js
+++ implementation/betweenTwoSets.test.js
@@ -5,5 +5,12 @@ test('testcase 0', () => {
 });
 
 test('testcase 1', () => {
+    expect(betweenTwoSets(
+        [100, 99, 98, 97, 96, 95, 94, 93, 92, 91], 
+        [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    )).toBe(0);
+});
+
+test('testcase 2', () => {
     expect(betweenTwoSets([3, 4], [24, 48])).toBe(2);
 });

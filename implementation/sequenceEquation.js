fbc942c7e4c6c4c6bcbef476e1d22cc2bcb2d84d
Pablo Lima Dias <pabloidz@gmail.com>
2018-11-06 22:16:41 -0200

    Usando Array.keys + spread

--- implementation/sequenceEquation.js
+++ implementation/sequenceEquation.js
@@ -1,5 +1,5 @@
 function sequenceEquation(numbers) {
     const p = num => numbers.indexOf(num) + 1;
-    return [...Array.from({length: numbers.length}).map((v, k) => k + 1)].map(p).map(p);
+    return [...numbers.keys()].map((v, k) => k + 1).map(p).map(p);
 }
 module.exports = sequenceEquation;

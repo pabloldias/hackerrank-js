ac0b876d7df580fb733684a8a24d19bb7878a94e
Pablo Lima Dias <pabloidz@gmail.com>
2018-11-27 23:56:05 -0200

    A better solution to Beautiful Triplets

--- implementation/beautifulTriplets.js
+++ implementation/beautifulTriplets.js
@@ -1,18 +1,4 @@
 function beautifulTriplets(d, arr) {
-    let count = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] - arr[i] === d) {
-                for (let k = j + 1; k < arr.length; k++) {
-                    if (arr[k] - arr[j] === d) {
-                        count++;
-                    }
-                }    
-            }
-        }    
-    }
-
-    return count;
+    return arr.filter(num => arr.includes(num + d) && arr.includes(num + 2 * d)).length
 }
 module.exports = beautifulTriplets;
